396eea71c524792e292c199da8faaa62ce8888c9
Anaëlle <anaelle.boyer.69@gmail.com>
2024-02-20 13:01:49 +0100

    b1 js fetch json 200224

--- cours.md
+++ cours.md
@@ -4,3 +4,83 @@
 
 ## Introduction à JavaScript
 
+## Sélectionner des éléments
+
+PAR ID
+```js
+let element = document.getElementById("monId");
+```
+
+PAR CLASSE
+``` js
+let elements = document.getElementsByClassName("maClasse");
+```
+
+## Modifier des éléments
+
+CHANGER LE TEXTE
+``` js
+document.getElementById("monId").textContent = "nouveau texte!";
+```
+
+CHANGER LE HTML INTERNE
+``` js
+document.getElementById("monId").innerHTML = "<span>Nouveau contenu HTML</span>;
+```
+
+## Modifier les styles
+
+CHANGER LE STYLE D'UN ELEMENT
+``` js
+document.getElementById("monId").style.color = "red";
+```
+
+## Ecouter et Réagir aux évènements
+
+AJOUTER UN ECOUTEUR D'EVENEMENTS
+``` js
+document.getElementById("monBouton").addEventListener("click", function () {
+    alert("Bouton cliqué !");
+});
+```
+
+## Fonctions
+``` js
+function nomDeLaFonction(param1, param2, param3)
+{
+    //Code à exécuter
+}
+```
+function -> callback : fonction passée en paramètre d'une autre fonction
+
+``` js
+const nomDeLaFonction = function (param1, param2, param3)
+{
+    //Code à exécuter
+}
+```
+
+``` js
+const nomDeLaFonction = (param1, param2, param3)
+{
+    //Code à exécuter
+}
+```
+
+## Event
+Le paramètre event est passé de façon implicite. Son nom peut être changé mais il est préférable de le laisser tel quel pour une meilleure compréhension.
+``` js
+form.onsubmit = (event) => {
+    alert ("test");
+}
+```
+
+## Chaînes de caractères
+
+Pour déclarer une string en js :
+
+``` js
+let str = "Hello World";
+let str = 'Hello World';
+
+```
--- index.js
+++ index.js
@@ -1,5 +1,80 @@
-function changerTitre() {
-    document.getElementById("titre").textContent = "Titre modifié !";
+//const monTitre = document.getElementById("titre");
+
+//Formualire
+const form = document.getElementById("form");
+
+// Select
+const select = document.getElementById("select");
+
+
+//Bouton additionner
+const addButton = document.getElementById("add");
+//addButton.addEventListener("click", function (){
+//    alert("ADDITION !");
+//});
+console.log(addButton);
+
+//Bouton soustraction
+const subButton = document.getElementById("sub");
+//susButton.addEventListener("click", function () {
+//    alert("SOUSTRACTION");
+//});
+console.log(susButton);
+
+// Bouton multiplier
+const mulButton = document.getElementById("mul");
+//mulButton.addEventListener("click", function () {
+//    alert("MULTIPLICATION");
+//});
+console.log(mulButton);
+
+// Bouton diviser
+const divButton = document.getElementById("div");
+//divButton.addEventListener("click", function () {
+//    alert("DIVISION");
+//});
+console.log(divButton);
+
+// Création de events
+form.onsubmit = (event) => {
+    event.preventDefault();
+    alert ("test");
+
+    // récupération de la valuer du select (add, sub, mul, div)
+    console.log("Valeur de l'élément select : ", select.value);
+
+    // récupération de la valuer des inputs
+    console.log("Valeur de l'élément input1 : ", input1.value);
+    console.log("Valeur de l'élément input2 : ", input2.value);
+
+    // utilisation de la valeur du select pour effectuer l'opération correspondante
+    switch (select.value) {
+        case "add":
+            console.log("calcul choisi addition");
+            // Aficher le résultat de l'addition
+            result.innerText = Number(input1.value) + Number(input2.value);
+            break;
+
+        case "sus":
+            console.log("calcul choisi soustraction");
+            console.log(Number(input1.value) - Number(input2.value));
+            break;
+
+        case "mul":
+            console.log("calcul choisi multiplication");
+            console.log(Number(input1.value) * Number(input2.value));
+            break;
+
+        case "div":
+            console.log("calcul choisi division");
+            console.log(Number(input1.value) / Number(input2.value));
+            break;
+
+        default:
+            console.log("calcul non géré");
+            break;
+    }
 }
 
-changerTitre();
+// Result
+const result = document.getElementById("result");
